53645f36c7ccacb83ac4930b4e3d0e7250cbdef2
adelguit <adeelguitoun@gmail.com>
2022-12-22 17:59:10 +0100

    Fix linters errors

--- app.rb
+++ app.rb
@@ -4,17 +4,16 @@ require_relative './models/book'
 require_relative './models/rental'
 
 class App
-
   def initialize
     # Print commands
     @cmd = {
-      "1": 'List all books',
-      "2": 'List all people',
-      "3": 'Create a person',
-      "4": 'Create a book',
-      "5": 'Create a rental',
-      "6": 'List all rentals for a given person id',
-      "7": 'Exit'
+      '1': 'List all books',
+      '2': 'List all people',
+      '3': 'Create a person',
+      '4': 'Create a book',
+      '5': 'Create a rental',
+      '6': 'List all rentals for a given person id',
+      '7': 'Exit'
     }
 
     # List of books
@@ -34,7 +33,7 @@ class App
   # Print out all books
   def list_all_books
     if @books.empty?
-      puts "Please insert books first!!"
+      puts 'Please insert books first!!'
     else
       @books.each_with_index do |book, index|
         puts "#{index}) Title: #{book.title}, Author: #{book.author}}"
@@ -45,7 +44,7 @@ class App
   # Print out all books
   def list_all_people
     if @people.empty?
-      puts "Please insert people first!!"
+      puts 'Please insert people first!!'
     else
       @people.each do |person|
         puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
@@ -59,133 +58,137 @@ class App
     end
   end
 
+  # Create Student
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp.downcase
+    case permission
+    when 'y' then parent_permission = true
+    when 'n' then parent_permission = false
+    end
+
+    # Create a student and push it to people array
+    @people.push(Student.new('11', age, name, parent_permission: parent_permission))
+  end
+
+  # Create teacher
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    # Create a student and push it to people array
+    @people.push(Teacher.new(specialization, age, name))
+  end
+
   # Create a person
   def create_person
-    print "Do you want to create a student (1) or a teacher (2)? [Input the nummber]: "
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the nummber]: '
     case gets.chomp
-      when "1"
-        print "Age: "
-        age = gets.chomp
+    when '1'
+      create_student
 
-        print "Name: "
-        name = gets.chomp
-
-        print "Has parent permission? [Y/N]: "
-        permission = gets.chomp.downcase
-        case permission
-          when "y" then parent_permission = true
-          when "n" then parent_permission = false
-        end
-
-        # Create a student and push it to people array
-        @people.push(Student.new("11", age, name, parent_permission: parent_permission))
-
-      when "2"
-        print "Age: "
-        age = gets.chomp
-
-        print "Name: "
-        name = gets.chomp
-
-        print "Specialization: "
-        specialization = gets.chomp
-
-        # Create a student and push it to people array
-        @people.push(Teacher.new(specialization, age, name))
+    when '2'
+      create_teacher
     end
 
     # Success message
-    puts "Person Created successfully"
+    puts 'Person Created successfully'
   end
 
   # Create a Book
   def create_book
-    print "Title: "
+    print 'Title: '
     title = gets.chomp
 
-    print "Author: "
+    print 'Author: '
     author = gets.chomp
 
     # Create the book object and add it to the books list
     @books.push(Book.new(title, author))
 
     # Success message
-    puts "Book Created successfully"
+    puts 'Book Created successfully'
   end
 
   # Create a rental
   def create_rental()
     # Select Book
-    puts "Select a book from the following list by number"
-    list_all_books()
+    puts 'Select a book from the following list by number'
+    list_all_books
     book_option = gets.chomp.to_i
     puts
 
     # Select Person
-    puts "Select a person from the following list by number (not id)"
-    list_all_person_with_numbers()
+    puts 'Select a person from the following list by number (not id)'
+    list_all_person_with_numbers
     person_option = gets.chomp.to_i
     puts
 
     # Select Date
-    print "Date: "
+    print 'Date: '
     date = gets.chomp
 
     # Create the book object and add it to the books list
     Rental.new(date, @books[book_option], @people[person_option])
 
     # Success Message
-    puts "Rental created successfully"
+    puts 'Rental created successfully'
   end
 
   # List all rentals for a given person id
   def rentals_of_person()
     # Select person
-    print "Id of person: "
+    print 'Id of person: '
     id = gets.chomp.to_i
-    person_arr = @people.select {|person| person.id == id}
+    person_arr = @people.select { |person| person.id == id }
 
     # Print rentals for that person
     if person_arr.empty?
-      puts "No person matches the given ID!!"
+      puts 'No person matches the given ID!!'
     else
       person_arr[0].rentals.each do |rental|
         puts "Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"
       end
     end
-
   end
 
-  # Applay user choosed option
-  def execute_user_option(user_option)
-    case user_option
-    when "1" then list_all_books()
-    when "2" then list_all_people()
-    when "3" then create_person()
-    when "4" then create_book()
-    when "5" then create_rental()
-    when "6" then rentals_of_person()
-    when "7" then exit
+  def choose_option(input)
+    case input
+    when '1' then list_all_books
+    when '2' then list_all_people
+    when '3' then create_person
+    when '4' then create_book
+    when '5' then create_rental
+    when '6' then rentals_of_person
+    end
+  end
+
+  def user_option_executer
+    input = gets.chomp
+
+    if input == '7'
+      exit
+    else
+      choose_option(input)
     end
   end
 
   # Run the program
   def run
     puts 'Please choose an option by entering a number:'
-    display_cmd()
-    user_option = gets.chomp
-    execute_user_option(user_option)
-    puts
-  end
-
-end
-
-# Main
-def main
-  app = App.new
-  loop do
-    app.run()
+    display_cmd
+    user_option_executer
   end
 end
-
-main()
